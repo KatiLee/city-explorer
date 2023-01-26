36aaca059842057101ff8f25361fd9a9915dedcd
KATI LEE <katherineleefoley@gmail.com>
2023-01-26 01:34:02 +0000

    trying an experiment to get npm load to work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 class App extends React.Component{
-
+  constructor(props){
+    super(props);
+    this.state = {
+        cityInfo: [],
+    }
+  }
   render(){
     
     
     return(
     <>
-    </>
+    <h1>City Info From LocationIQ</h1>
+    </>;
 
 
 
